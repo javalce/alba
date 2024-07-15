ee625641784b4ecadcf2adb5c1d5aa88b98c80ea
javalce <javalce29@gmail.com>
2024-07-15 14:38:30 +0200

    refactor: rename CLI command from "create" to "init"

--- src/cli/database.py
+++ src/cli/database.py
@@ -5,7 +5,7 @@ from src.database.database import Database
 app = Typer(rich_markup_mode="rich")
 
 
-@app.command("create")
+@app.command("init")
 def create_schema():
     """
     Creates the milvus database schema.

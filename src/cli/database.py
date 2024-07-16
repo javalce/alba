71492a2a593698abddaaaaf71d3453f890e0eb6c
javalce <javalce29@gmail.com>
2024-07-16 09:48:16 +0200

    feat: add CLI command to add documents to the database

--- src/cli/database.py
+++ src/cli/database.py
@@ -1,4 +1,7 @@
-from typer import Typer
+from pathlib import Path
+from typing import Annotated
+
+from typer import Argument, Typer
 
 from src.database import Database
 
@@ -11,7 +14,6 @@ def initialize_database_schema():
     Creates the milvus database schema.
     """
     db = Database()
-
     db.initialize()
 
 
@@ -21,5 +23,28 @@ def clear_database_schema():
     Drops the milvus database schema.
     """
     db = Database()
-
     db.clear_database()
+
+
+@app.command("add")
+def add_document(
+    files: Annotated[
+        list[Path],
+        Argument(
+            help="The file paths to the document to be added.",
+            exists=True,
+            file_okay=True,
+            dir_okay=False,
+            writable=False,
+            readable=True,
+            resolve_path=True,
+        ),
+    ]
+):
+    """
+    Adds a document to the database.
+    """
+    files = [str(file) for file in files]
+
+    db = Database()
+    db.add_documents(files)

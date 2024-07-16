2d80c861789127ce903f5fe0380c4d437ec13209
javalce <javalce29@gmail.com>
2024-07-16 11:36:52 +0200

    feat: add error handling for document upload

--- src/alba/router/document.py
+++ src/alba/router/document.py
@@ -9,4 +9,9 @@ router = APIRouter(prefix="/documents", tags=["document"])
 def add_document(files: list[UploadFile], request: Request):
     chatbot: Chatbot = request.app.state.chatbot
 
-    chatbot.long_term_mem.db.add_document(files)
+    try:
+        chatbot.long_term_mem.db.add_documents(files)
+    except Exception:
+        return {"message": "There was an error while uploading the documents"}
+
+    return {"message": f"Succesfully added {[file.filename for file in files]}"}

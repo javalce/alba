10d8568c5b5ff4494c200e45b8574fe1145b42e0
javalce <javalce29@gmail.com>
2024-07-24 13:18:51 +0200

    feat: update document service to handle file objects and filenames

--- src/alba/services.py
+++ src/alba/services.py
@@ -10,24 +10,24 @@ class DocumentService:
     def __init__(self, repository: repositories.DocumentRepository):
         self.repository = repository
 
-    def add_document(self, file: UploadFile):
-        document = models.Document(name=file.filename, hash_value=self.create_sha256(file))
-        self.repository.save(document)
-
-    def add_documents(self, files: list[UploadFile]):
+    def add_documents(self, files: list[UploadFile | str]):
         documents = [
             models.Document(name=file.filename, hash_value=self.create_sha256(file))
             for file in files
         ]
         self.repository.save_all(documents)
 
-    def create_sha256(self, file: UploadFile):
-        f = file.file
-        data = f.read()
+    def create_sha256(self, file: UploadFile | str):
+        if isinstance(file, str):
+            with open(file, "rb") as f:
+                data = f.read()
+        else:
+            f = file.file
+            data = f.read()
 
         return hashlib.sha256(data).hexdigest()
 
-    def verify_document(self, file: UploadFile):
+    def verify_document(self, file: UploadFile | str):
         sha256_returned = self.create_sha256(file)
         document = self.repository.find_by_hash(sha256_returned)
 
--- src/alba_cli/database.py
+++ src/alba_cli/database.py
@@ -2,28 +2,62 @@ from pathlib import Path
 from typing import Annotated
 
 from alba.milvus_database import MilvusDatabase
-from dependency_injector.wiring import Provide
+from alba.services import DocumentService
+from dependency_injector.wiring import Provide, inject
+from sqlalchemy_toolkit import DatabaseManager, Entity
 from typer import Argument, Typer
 
 app = Typer(rich_markup_mode="rich")
 
-db: MilvusDatabase = Provide["db"]
+
+db: DatabaseManager = Provide["db"]
+document_service = Provide["document_service"]
+milvus_db: MilvusDatabase = Provide["milvus_db"]
+
+
+@inject
+def __init_database(
+    db: DatabaseManager = Provide["db"],
+    milvus_db: MilvusDatabase = Provide["milvus_db"],
+):
+    Entity.metadata.create_all(db.engine)
+    milvus_db.initialize()
+
+
+@inject
+def __clear_database(
+    db: DatabaseManager = Provide["db"],
+    milvus_db: MilvusDatabase = Provide["milvus_db"],
+):
+    Entity.metadata.drop_all(db.engine)
+    milvus_db.clear_database()
+
+
+@inject
+def __add_document(
+    files: list[str],
+    document_service: DocumentService = Provide["document_service"],
+    milvus_db: MilvusDatabase = Provide["milvus_db"],
+):
+    files = [file for file in files if not document_service.verify_document(file)]
+    document_service.add_documents(files)
+    milvus_db.add_documents(files)
 
 
 @app.command("init")
-def initialize_database_schema():
+def initialize_database():
     """
     Creates the milvus database schema.
     """
-    db.initialize()
+    __init_database()
 
 
 @app.command("drop")
-def clear_database_schema():
+def clear_database():
     """
     Drops the milvus database schema.
     """
-    db.clear_database()
+    __clear_database()
 
 
 @app.command("add")
@@ -45,8 +79,7 @@ def add_document(
     Adds a document to the database.
     """
     files = [str(file) for file in files]
-
-    db.add_documents(files)
+    __add_document(files)
 
 
 @app.command("reset")
@@ -54,5 +87,5 @@ def reset_database():
     """
     Resets the database.
     """
-    db.clear_database()
-    db.initialize()
+    __clear_database()
+    __init_database()
